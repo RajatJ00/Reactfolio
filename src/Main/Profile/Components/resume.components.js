a7963821b23cbbbcbf1dd6964c20996c895824f3
RajatJ00 <rajatjamgade00@gmail.com>
2025-03-22 17:27:20 +0530

    Updated Successfully!!!

--- src/Main/Profile/Components/resume.components.js
+++ src/Main/Profile/Components/resume.components.js
@@ -1,10 +1,8 @@
-import {FaFileDownload} from "react-icons/fa";
-
 function ResumeButton() {
     return (
-        <a className="hover:text-neutral-50 w-fit cursor-pointer rounded-xl flex gap-2 px-6 py-2 text-base font-medium uppercase my-6 h-10 bg-white/0 hover:bg-white/10 shadow-[inset_1.5px_1.5px_4px_0px_#000] active:shadow-[inset_-1.5px_-1.5px_4px_1px_#000] justify-center items-center overflow-hidden relative group transition duration-150 ease-in-out text-transparent bg-gradient-to-r from-red-600 to-blue-700 bg-clip-text" href="Rajat.pdf" download="Rajat_RESUME.pdf" title="Rajat_Resume">
-            Resume<FaFileDownload className="text-neutral-50"/>
-            <img src="graph.png" alt="graphics_shadow" className="absolute -bottom-[100px] scale-[1.2] blur-md -z-10 group-hover:animate-ping select-none" />
+        <a className="w-fit cursor-pointer rounded-xl flex gap-2 px-6 py-2 text-base font-extrabold uppercase my-6 h-10 shadow-[1.5px_1.5px_4px_0px_#000] hover:shadow-[inset_1.5px_1.5px_4px_0px_#000] active:shadow-[inset_-1.5px_-1.5px_4px_1px_#000] justify-center overflow-hidden relative text-transparent bg-gradient-to-r from-red-600 to-blue-700 bg-clip-text" href="Rajat.pdf" download="Rajat_RESUME.pdf" title="Rajat_Resume">
+            Resume
+            <img src="graph.png" alt="graphics_shadow" className="absolute -bottom-[86px] bg-black/30 scale-[1.2] blur -z-10 select-none" />
         </a>
     )
 }
--- src/Main/Projects/Components/ProjectInfo.components.js
+++ src/Main/Projects/Components/ProjectInfo.components.js
@@ -1,3 +1,4 @@
+import { FaArrowDown, FaGithub } from "react-icons/fa";
 import ProjectLink from "./ProjectLink";
 
 function ProjectInfo(props) {
@@ -5,15 +6,16 @@ function ProjectInfo(props) {
         <>
             <ul className="w-fit gap-1 font-normal flex flex-wrap justify-center">
                 {props.data.map((value, index) => (
-                    <li key={index} className="lg:w-80 max-h-56 s_phone:w-fit lg:mx-12 s_phone:mx-0 mt-10 cursor-pointer hover:text-black text-lg overflow-hidden rounded-xl bg-white/0 hover:bg-white/5 shadow-md shadow-black backdrop-blur-md transition duration-300 ease-in-out group">
+                    <li key={index} className="lg:w-80 max-h-56 s_phone:w-fit lg:mx-12 s_phone:mx-0 mt-10 hover:text-black text-lg overflow-hidden rounded-xl bg-white/0 hover:bg-white/5 shadow-md shadow-black backdrop-blur-md transition duration-300 ease-in-out group">
 
                         {/* Project Live Link */}
                         <ProjectLink title={value.title} href={value.live} />
 
-                        <p className="text-neutral-50 duration-[1500ms] ease-in-out transition-all hidden group-hover:block m-4 line-clamp-1 hover:line-clamp-none text-center lg:text-base">{value.skills}</p>
-                        <a title={value.title} href={value.github}>
+                        <p className="text-neutral-50 duration-[1500ms] ease-in-out transition-all hidden group-hover:block m-3 line-clamp-1 hover:line-clamp-none text-center lg:text-base">{value.skills}
+                        </p>
+                        <a title={value.title} href={value.github} className="relative">
+                            <span className="absolute top-0 bg-black/80 z-10 flex items-center place-self-center gap-3 w-fit rounded-b-xl px-2 py-2 mx-2 ml-2 text-xs uppercase shadow-[-1.5px_-1.5px_4px_0px_#000] text-neutral-50 justify-self-center font-Name"><FaGithub /></span>
                             <img src={value.image} alt={value.title} className="relative" />
-                            <p className="absolute flex justify-self-center">{value.title}</p>
                         </a>
                     </li>
                 ))}
