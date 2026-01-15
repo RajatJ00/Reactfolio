8e1a4503a44d57800a04a8aa4da2f34f578b5aa4
Rajat Jamgade <rajatjamgade00@gmail.com>
2026-01-16 01:14:15 +0530

    Updated!!!

--- src/Main/Profile/Components/resume.components.js
+++ src/Main/Profile/Components/resume.components.js
@@ -1,6 +1,6 @@
 function ResumeButton() {
     return (
-        <a className="w-fit cursor-pointer rounded-xl flex gap-2 px-6 py-2 text-base font-extrabold uppercase my-6 h-10 shadow-[1.5px_1.5px_4px_0px_#000] hover:shadow-[inset_1.5px_1.5px_4px_0px_#000] active:shadow-[inset_-1.5px_-1.5px_4px_1px_#000] justify-center overflow-hidden relative text-transparent bg-gradient-to-r from-color1 active:from-red-600 active:to-blue-700 bg-clip-text" href="Rajat.pdf" download="Rajat_RESUME.pdf" title="Rajat_Resume">
+        <a className="w-fit cursor-pointer rounded-xl flex gap-2 px-6 py-2 text-base font-extrabold uppercase my-6 h-10 shadow-[1.5px_1.5px_4px_0px_#000] hover:shadow-[inset_1.5px_1.5px_4px_0px_#000] active:shadow-[inset_-1.5px_-1.5px_4px_1px_#000] justify-center overflow-hidden relative text-transparent bg-gradient-to-r from-color1 active:text-color1 bg-clip-text" href="Rajat.pdf" download="Rajat_RESUME.pdf" title="Rajat_Resume">
             Resume
             <img src="graph.png" alt="graphics_shadow" className="absolute -bottom-[86px] bg-black/30 scale-[1.2] blur -z-10 select-none" />
         </a>
--- src/Main/Profile/Profile.js
+++ src/Main/Profile/Profile.js
@@ -8,14 +8,14 @@ function ProfilePic() {
     <>
       <div className="flex justify-evenly pt-16 pb-3 gap-4 w-full lg:px-14 s_phone:px-2 font-Name">
         {/* Profile Pic */}
-        <div className="lg:w-[22%] s_phone:w-[37%] h-fit relative flex self-center">
+        <div className="lg:w-[18%] s_phone:w-[38%] h-fit relative flex self-center">
           <Photo />
         </div>
 
         <div className="lg:mr-24 lg:px-12 pt-5 lg:w-[57%] lg:h-fit s_phone:w-[63%] s_phone:h-72 s_phone:px-6 rounded-xl hover:bg-white/[0.02] backdrop-blur-sm transition duration-300 ease-in-out group">
 
           <Intro />
-          <div className=" absolute bottom-0">
+          <div className="lg:relative s_phone:absolute bottom-0">
             <SocialMedia />
             <ResumeButton />
           </div>
